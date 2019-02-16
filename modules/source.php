b327e3b948d3167b65f749275ff0cf7fab89444a
Tobias Rojahn <tobias@rojahn.org>
2019-02-15 21:31:45 -0800

    deletable source files

--- modules/source.php
+++ modules/source.php
@@ -4,6 +4,10 @@ if (isset($_POST['source'])) {
 	file_put_contents(PROJECT . $id, $_POST['source']);
 }
 
+if (isset($_GET['delete'])) {
+    unlink(PROJECT . $id);
+}
+
 $view->assign('template', 'source.twig');
 $view->assign('source', file_get_contents(PROJECT . $id));
 $view->assign('action', '?modul=source&id=' . $id);
